83548ca69262ff5db894e84bd6e4fa41f13e456f
jmartin <jmartin@zikzakmedia.com>
2016-03-02 12:30:34 +0100

    Implements generate_gestmag_sga on product model

--- product.py
+++ product.py
@@ -1,6 +1,9 @@
 #This file is part gestmag_sga module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
+from datetime import datetime
+from trytond.model import ModelView, fields
+
 from trytond.pool import Pool, PoolMeta
 from trytond.model import ModelView, fields
 from trytond.wizard import Wizard, StateTransition, StateView, Button
@@ -15,33 +18,79 @@ class Product:
 
     @classmethod
     def create(cls, vlist):
-        Gestmag = Pool().get('gestmag')
-
         products = super(Product, cls).create(vlist)
-
-        gestmag_products = []
-        for product in products:
-            if product.code:
-                gestmag_products.append(product)
-        if gestmag_products:
-            Gestmag.export_products(gestmag_products)
+        cls.generate_gestmag_sga(products)
         return products
 
     @classmethod
     def write(cls, *args):
-        Gestmag = Pool().get('gestmag')
-
         super(Product, cls).write(*args)
-
-        gestmag_products = []
         actions = iter(args)
-        for products, values in zip(actions, actions):
-            # generate xml if code or codes changed
-            if values.get('code') or values.get('codes'):
-                gestmag_products = products
+        for products, _ in zip(actions, actions):
+            cls.generate_gestmag_sga(products)
 
-        if gestmag_products:
-            Gestmag.export_products(gestmag_products)
+    @classmethod
+    def generate_gestmag_sga(cls, products):
+        pool = Pool()
+        Gestmag = pool.get('gestmag')
+        Company = pool.get('company.company')
+
+        company = Company(Transaction().context.get('company'))
+        company_code = company.party.code
+        today = datetime.today().strftime('%d%m%Y')
+
+        gestmags = Gestmag.search([
+            ('name', '=', 'EXPORT_PRODUCT'),
+            ])
+        if gestmags:
+            gestmag = gestmags[0]
+            headers = [
+                'EMPRESA',
+                'CODFAM',
+                'CODSUBFAM',
+                'CODART',
+                'DESCR',
+                'EAN_ART',
+                'EAN_UV',
+                'TIPO_UT',
+                'UTxUV',
+                'T_PAL',
+                'UxP',
+                'PBxU',
+                'C_ALTO',
+                'C_ANCHO',
+                'C_FONDO',
+                'F_ACT',
+                ]
+            rows = []
+            for product in products:
+                template = product.template
+                rows.append([
+                        company_code.encode('utf-8'),
+                        template.category.parent.name.encode('utf-8')
+                            if template.category and template.category.parent
+                            else '',
+                        template.category.name.encode('utf-8')
+                            if template.category else '',
+                        product.code.encode('utf-8') if product.code else '',
+                        template.name.encode('utf-8'),
+                        product.code_ean13 or ''
+                        '',  # Not implemented
+                        template.default_uom.name,
+                        template.sale_uom.factor
+                            if template.sale_uom and template.sale_uom.factor
+                            else '',
+                        template.sale_uom.name
+                            if template.sale_uom else '',
+                        0,  # Not implemented
+                        0,  # Not implemented
+                        0,  # Not implemented
+                        0,  # Not implemented
+                        0,  # Not implemented
+                        0,  # Not implemented
+                        today,
+                        ])
+            gestmag.export_file(rows, headers)
 
 
 class GestmagProductResult(ModelView):
@@ -71,11 +120,10 @@ class GestmagProduct(Wizard):
     def transition_export(self):
         pool = Pool()
         Product = pool.get('product.product')
-        Gestmag = pool.get('gestmag')
 
         products = Product.browse(Transaction().context['active_ids'])
-        Gestmag.export_products(products)
-        print "ara aqui"
+        Product.generate_gestmag_sga(products)
+
         self.result.info = self.raise_user_error('export_info',
             (len(products)), raise_exception=False)
         return 'result'
